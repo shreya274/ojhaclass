92f77df395fd2b977a1a4731bbcce64507cecaa1
shreya274 <shreyasharmass274@gmail.com>
2024-08-20 12:17:38 +0530

    2

--- file1.js
+++ file1.js
@@ -1,5 +1,24 @@
-let msg=()=>{
+//let msg=()=>{
     document.write("happy birthday<br>")
+//}
+//msg()
+//msg()
+//let str1="hello gla"
+//console.log(str1);
+//let str2=new String("hello")
+//console.log(str2);
+var students={
+    id:101,
+    sname:"kapil",
+    ispassed:true,
+    
 }
-msg()
-msg()
+//console.log(students);
+let show=`<div style="text-align:center">
+ 
+<h1 style="background:tomato;color:white;"> student name:${students.sname}</h1><hr>
+<h2>id:${students.id}</h2>
+<h2>passed:${students.ispassed? "pass out":"pursuing"}
+</div>
+`
+document.write(show)
